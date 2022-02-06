d5623e4ff34bc0cb32b0b45f7f145a5a71e26a42
M1ker1n <35259381+m1ker1n@users.noreply.github.com>
2022-02-06 06:06:34 +0300

    Revert "Merge pull request #1 from m1ker1n/undone" (#2)
    This reverts commit d7ab72e9fba59bc6fd9d3c24d35cf9b4b14c0a8e, reversing
    changes made to 3e1d449005a5cd8f22ce89d97c01397cede14278.

---
+++ Square into Squares. Protect trees!/Square into Squares. Protect trees!/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Square_into_Squares._Protect_trees_
+{
+
+    static public class Decompose
+    {
+        static public List<long> list = new List<long>();
+        static public long decomposeRecursive(long left, long prevIndex)
+        {
+            if (left < 0) return -1; // too much for previous index
+            if (prevIndex <= 0) return -1; // no more indexes to iterate
+            if (left == 0)
+            {
+                list.Add(prevIndex);
+                return prevIndex; //found solution
+            }
+
+            for (long i = prevIndex - 1; i > 0; i--) 
+            {
+                if (decomposeRecursive(left - i * i, i) != -1)
+                {
+                    list.Add(prevIndex);  //found solution
+                    return prevIndex;
+                }
+            }
+
+            return -1;
+        }
+
+        static public string decompose(long n)
+        {
+            decomposeRecursive(n * n, n);
+
+            if (list.Count == 0) return "Nothing";
+
+            string result = "";
+            list.Remove(n);
+            foreach (var item in list)
+            {
+                result += item;
+                result += ' ';
+            }
+            result = result.Trim();
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decompose.decompose(11));
+        }
+    }
+}

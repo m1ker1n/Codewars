1f73198060df638dee36f422db024f23d4723d87
M1ker1n <m1ker1nigor@yandex.ru>
2022-02-17 08:15:45 +0300

    Before CodewarsTesting

--- Codewars style ranking system/Codewars style ranking system/User.cs
+++ Codewars style ranking system/Codewars style ranking system/User.cs
@@ -10,6 +10,7 @@ namespace Codewars_style_ranking_system
     {
         private static int[] rankArray =
             {-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8};
+        private int currentRankIndex;
         private static int progressLimit = 100;
 
         public int rank { get; private set; }
@@ -17,21 +18,68 @@ namespace Codewars_style_ranking_system
 
         public User()
         {
-            rank = rankArray[0];
-            progress = 0;
+            this.currentRankIndex = 0;
+            this.rank = rankArray[currentRankIndex];
+            this.progress = 0;
         }
 
         protected User(int rank, int progress)
         {
+            this.currentRankIndex = FindIndex(rank);
             this.rank = rank;
             this.progress = progress;
         }
 
-        public int incProgress(int activityRank)
+        public void incProgress(int activityRank)
         {
-            int result = 0;
+            if (!IsValidRank(activityRank)) throw new ArgumentException("Activity rank doesn't exist");
 
-            return result;
+            int differenceInRankings = FindIndex(activityRank) - this.currentRankIndex;
+
+            if (differenceInRankings < -1) return;
+
+            int progressToReceive = 0;
+            switch(differenceInRankings)
+            {
+                case (-1):
+                    progressToReceive = 1;
+                    break;
+                case (0):
+                    progressToReceive = 3;
+                    break;
+                default:
+                    progressToReceive = 10 * differenceInRankings * differenceInRankings;
+                    break;
+            }
+
+            this.progress += progressToReceive;
+            TryRankUp();
+        }
+
+        private void TryRankUp()
+        {
+            while(this.progress>=progressLimit)
+            {
+                if (this.currentRankIndex == rankArray.Length - 1) return;
+                this.progress -= progressLimit;
+                this.currentRankIndex++;
+                this.rank = rankArray[this.currentRankIndex];
+            }
+        }
+
+        private int FindIndex(int rank)
+        {
+            int index = -1;
+
+            for (int i = 0; i < rankArray.Length; i++)
+                if (rankArray[i] == rank) return i;
+
+            return index;
+        }
+
+        private bool IsValidRank(int activityRank)
+        {
+            return (activityRank != 0 && activityRank >= rankArray[0] && activityRank <= rankArray[rankArray.Length - 1]);
         }
     }
 }
